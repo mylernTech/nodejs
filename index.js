254da04541b2487206b453379c9429c3cbb7ee50
Benz Stevox <benzstevox@windows>
2019-08-01 14:54:42 +0300

    Added website copy and created server

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// TODO Add server code here
+const express = require('express');
+const app = express();
+const port = 8000
+const path = require('path')
+
+app.use('/', express.static('ui'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './ui/index.html'));
+});
+
+//TODO create about page
+//TODO create portifolio page
+// TODO add handler for contact form
+
+app.listen(port, (err) => {
+    if (!err){
+        console.log(`Server started on port ${port}`);
+    }
+})
